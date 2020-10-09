337ddae831a7a7a19b3957a3e404b394396e33bd
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-10-10 00:00:48 +1300

    [master] adds config-files dir and scripts for cloudwatch agent

--- config-files/setup-cloudWatch-agent.sh
+++ config-files/setup-cloudWatch-agent.sh
@@ -4,6 +4,8 @@ echo "Setting up Unified CloudWatch Agent..."
 SERVER_CONTAINER_ID=$(docker ps | grep atomic-coconut-server | awk '{print $1}')
 echo "aCo server container id: $SERVER_CONTAINER_ID"
 sed -i "s?server-CONTAINER_ID-stdouterr?server-$SERVER_CONTAINER_ID-stdouterr?" cloudWatchAgentConfig.json
+echo
+echo "Setup cloudWatch agent..."
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:cloudWatchAgentConfig.json -s
 
 echo "CloudWatch Agent setup done."
