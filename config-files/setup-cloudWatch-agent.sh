490c2204d4ba80ab93af525d2111e05d0be35604
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-10-10 00:08:49 +1300

    [master] cloudwatch agent - adds a directory and file neccessary to start it

--- config-files/setup-cloudWatch-agent.sh
+++ config-files/setup-cloudWatch-agent.sh
@@ -6,6 +6,8 @@ echo "aCo server container id: $SERVER_CONTAINER_ID"
 sed -i "s?server-CONTAINER_ID-stdouterr?server-$SERVER_CONTAINER_ID-stdouterr?" cloudWatchAgentConfig.json
 echo
 echo "Setup cloudWatch agent..."
+sudo mkdir -p /usr/share/collectd
+sudo touch /usr/share/collectd/types.db
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:cloudWatchAgentConfig.json -s
 
 echo "CloudWatch Agent setup done."
