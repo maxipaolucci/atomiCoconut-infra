0c84a6b0578ca27985e783b8f6ce9342b8b25523
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-10-10 18:59:10 +1300

    [master] adds cloudwatch group creation

--- config-files/setup-cloudWatch-agent.sh
+++ config-files/setup-cloudWatch-agent.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 echo "Setting up Unified CloudWatch Agent..."
+
 SERVER_CONTAINER_ID=$(docker ps | grep atomic-coconut-server | awk '{print $1}')
 echo "aCo server container id: $SERVER_CONTAINER_ID"
+
+ENVIRONMENT_TYPE=$(sudo /opt/elasticbeanstalk/bin/get-config environment | jq .ENVIRONMENT_TYPE | tr -d '"')
+if [[ -z "$ENVIRONMENT_TYPE" ]]; then ENVIRONMENT_TYPE=production; fi
+echo "Environment type: $ENVIRONMENT_TYPE"
+
 sed -i "s?server-CONTAINER_ID-stdouterr?server-$SERVER_CONTAINER_ID-stdouterr?" cloudWatchAgentConfig.json
+sed -i "s?aco-ENVIRONMENT_TYPE?aco-$ENVIRONMENT_TYPE?" cloudWatchAgentConfig.json
 echo
+
 echo "Setup cloudWatch agent..."
 sudo mkdir -p /usr/share/collectd
 sudo touch /usr/share/collectd/types.db
