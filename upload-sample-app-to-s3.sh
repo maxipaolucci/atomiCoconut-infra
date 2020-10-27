8f853cc319bfbaa43123f2dc572fe4c495cdc1fd
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-10-28 10:55:27 +1300

    [master] migrates to amazon linux 2 - fix bug with upload ecs-sample script

--- upload-sample-app-to-s3.sh
+++ upload-sample-app-to-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# zip the app
-cd ecs-sample; zip -r ../ecs-sample.zip * .ebext*
+# zip the app including .ebext* and .plat* dirs
+cd ecs-sample; zip -r ../ecs-sample.zip * .ebext* .plat*
 cd ..
 
 # upload zip to S3
