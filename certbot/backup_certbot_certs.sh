1753752b790eb865f80eafb2e42cd408a8973ec9
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-11-01 15:22:17 +1300

    [master] upload certificates backup with latest tag

--- certbot/backup_certbot_certs.sh
+++ certbot/backup_certbot_certs.sh
@@ -11,8 +11,10 @@ echo "### Environment: $environment"
 # compress conf directory in a tar.gz file with today date
 sudo tar -czvf certbot-certs-$todayDate.tar.gz conf www
 
-# backup/upload tar file to S3 bucket (sse AES256 encryption and intelligent tiering)
+# backup/upload tar files to S3 bucket (sse AES256 encryption and intelligent tiering)
 aws s3 cp ./certbot-certs-$todayDate.tar.gz s3://$1/atomiCoconut$environment/certbot_certs_backups/certbot-certs-$todayDate.tar.gz --sse --storage-class INTELLIGENT_TIERING
+# tag latest
+aws s3 cp ./certbot-certs-$todayDate.tar.gz s3://$1/atomiCoconut$environment/certbot_certs_backups/certbot-certs-latest.tar.gz --sse --storage-class INTELLIGENT_TIERING
 
 # remove local tar.gz file already backup
 sudo rm -f certbot-certs-$todayDate.tar.gz
