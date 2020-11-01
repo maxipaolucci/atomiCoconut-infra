9e01f3b4ec4d348b2fc7fd0b283c2cc17369c632
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-11-01 14:07:18 +1300

    [master] better template for scripts

--- certbot/request_letsencrypt_certs.sh
+++ certbot/request_letsencrypt_certs.sh
@@ -1,18 +1,83 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Title
+#          Description
+# ------------------------------------------------------------------
 
+VERSION=0.1.0
+SUBJECT=request-letsencrypt-certs
+
+function print_usage()
+{
+    echo "Usage: request-letsencrypt-certs.sh [-h] STAGING ENVIRONMENT"
+  	echo -e "Parameters: \n\t STAGING: 1 for test letsencrypt api, 0 for real action \n\t ENVIRONMENT: use \"\" for production or \"testing\" for testing environment\n"
+    echo -e "\n Use -h for help"
+}
+
+# --- Options processing -------------------------------------------
+if [ $# == 0 ] ; then
+    print_usage
+    exit 1;
+fi
+
+while getopts ":vh" optname
+  do
+    case "$optname" in
+      "v")
+        echo "Version $VERSION"
+        exit 0;
+        ;;
+      # "i")
+      #   echo "-i argument: $OPTARG"
+      #   ;;
+      "h")
+        print_usage
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+shift $(($OPTIND - 1))
+
+param1=$1
+param2=$2
+
+# --- Locks -------------------------------------------------------
+LOCK_FILE=/tmp/$SUBJECT.lock
+if [ -f "$LOCK_FILE" ]; then
+   echo "Script is already running"
+   exit
+fi
+
+trap "rm -f $LOCK_FILE" EXIT
+touch $LOCK_FILE
+
+# --- Body --------------------------------------------------------
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
-if [[ ! -z "$1" ]]; then staging=$1; fi
+if [[ ! -z "$param1" ]]; then staging=$param1; fi
 echo "### Staging: $staging"
 
 domains=(atomicoconut.com www.atomicoconut.com)
-if [[ "$2" == "testing"  ]]; then
+if [[ "$param2" == "testing"  ]]; then
   domains=(testingss.atomicoconut.com)
 fi
 
 #this is used to restart nginx the container to reload the certs
 nginxImageTag=""
-if [[ ! -z "$2" ]]; then
-	nginxImageTag=$2;
+if [[ ! -z "$param2" ]]; then
+	nginxImageTag=$param2;
 fi
 
 rsa_key_size=4096
@@ -59,3 +124,4 @@ echo
 echo 
 echo
 ./start_certbot_renew.sh
+# -----------------------------------------------------------------
--- certbot/retrieve-letsencrypt-from-s3-bkp.sh
+++ certbot/retrieve-letsencrypt-from-s3-bkp.sh
@@ -1,25 +1,89 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Title
+#          Description
+# ------------------------------------------------------------------
 
+VERSION=0.1.0
+SUBJECT=retrieve-letsencrypt-from-s3-bkp
+
+function print_usage()
+{
+    echo "Usage: retrieve-letsencrypt-from-s3-bkp.sh [-h] ENVIRONMENT BKP_DATE"
+  	echo -e "Parameters: \n\t ENVIRONMENT: use \"\" for production \n\t BKP_DATE: format as YYMMDD \n"
+    echo -e "\n Use -h for help"
+}
+
+# --- Options processing -------------------------------------------
+if [ $# == 0 ] ; then
+    print_usage
+    exit 1;
+fi
+
+while getopts ":vh" optname
+  do
+    case "$optname" in
+      "v")
+        echo "Version $VERSION"
+        exit 0;
+        ;;
+      # "i")
+      #   echo "-i argument: $OPTARG"
+      #   ;;
+      "h")
+        print_usage
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+shift $(($OPTIND - 1))
+
+param1=$1
+param2=$2
+
+# --- Locks -------------------------------------------------------
+LOCK_FILE=/tmp/$SUBJECT.lock
+if [ -f "$LOCK_FILE" ]; then
+   echo "Script is already running"
+   exit
+fi
+
+trap "rm -f $LOCK_FILE" EXIT
+touch $LOCK_FILE
+
+# --- Body --------------------------------------------------------
 # capture environment parameter if present
 environment="" # Set to "testing" if you're in testing env
 nginxImageTag="" # this is used to restart the nginx container to reload the certs
-if [[ ! -z "$1" ]]; then
-	environment="-$1";
-	nginxImageTag=$1;
+if [[ ! -z "$param1" ]]; then
+	environment="-$param1";
+	nginxImageTag=$param1;
 fi
 echo "### Retrieving bkp from Environment: atomiCoconut$environment"
 
-if [[ -z "$2" ]]; then
-	  echo "Usage: retrieve-letsencrypt-from-s3-bkp.sh [ENVIRONMENT] [BKP_DATE]"
-  	echo -e "Parameters: \n\t ENVIRONMENT: use \"\" for production \n\t BKP_DATE: format as YYMMDD \n"
-  	exit 1;
+if [[ -z "$param2" ]]; then
+	  print_usage
+  	exit 0;
 fi
 
-aws s3 cp s3://elasticbeanstalk-ap-southeast-2-782522910439/atomiCoconut$environment/certbot_certs_backups/certbot-certs-$2.tar.gz .
+aws s3 cp s3://elasticbeanstalk-ap-southeast-2-782522910439/atomiCoconut$environment/certbot_certs_backups/certbot-certs-$param2.tar.gz .
 
-tar xvfz certbot-certs-$2.tar.gz
+tar xvfz certbot-certs-$param2.tar.gz
 
-rm certbot-certs-$2.tar.gz
+rm certbot-certs-$param2.tar.gz
 
 
 echo
@@ -30,3 +94,4 @@ echo
 echo
 ./start_certbot_renew.sh
 
+# -----------------------------------------------------------------
