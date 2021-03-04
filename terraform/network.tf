fe786d2761cc2fb90f8f2fc6ecb8f360ad83917f
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-03-04 14:45:41 +1300

    [master] adds route53 record

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,6 +2,8 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Security group for Elastic Beanstalk environment
+##################################################
 resource "aws_security_group" "atomicoconut_security_group" {
   name        = "atomiCoconut-${var.environment}-SecurityGroup"
   description = "Security Group for elasticbeanstalk ${var.environment} environment - ${var.app_name}"
@@ -33,4 +35,19 @@ resource "aws_security_group" "atomicoconut_security_group" {
   tags = {
     NAME = "${var.app_name}-security-group"
   }
+}
+
+# Route 53 record to point to Elastic Beanstalk
+################################################
+
+data "aws_route53_zone" "atomicoconut" {
+  name         = "atomicoconut.com."
+}
+
+resource "aws_route53_record" "r53_to_elastic_beanstalk_dns_record" {
+  zone_id = data.aws_route53_zone.atomicoconut.zone_id
+  name    = var.environment == "production" ? "atomicoconut.com" : "${var.environment}ss.atomicoconut.com"
+  type    = "A"
+  ttl     = "3600"
+  records = [aws_elastic_beanstalk_environment.elasticbeanstalk_environment.endpoint_url]
 }
