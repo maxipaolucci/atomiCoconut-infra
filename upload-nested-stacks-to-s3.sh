9f69603d07293d3b30828d568d7b08cecd4d19b9
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-03-17 00:34:08 +1300

    [nested-stacks] adds cicd as a nested stack for eb env script

--- upload-nested-stacks-to-s3.sh
+++ upload-nested-stacks-to-s3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# upload all in nested-stacks dir to s3
+# upload all generic nested-stacks dir to s3
 aws s3 cp ./nested-stacks/ s3://maxipaolucci/cloudformation-stacks/ --recursive --sse --storage-class INTELLIGENT_TIERING
+
+# upload atomiCoconut nested-stacks dir to s3
+aws s3 cp ./nested-stacks-aco/ s3://atomicoconut/infrastructure/nested-stacks/ --recursive --sse --storage-class INTELLIGENT_TIERING
 
