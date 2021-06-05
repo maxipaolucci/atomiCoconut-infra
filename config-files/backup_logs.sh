9e9e451cfff109a1896f12a06e07d86b15a53b73
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-06-05 17:49:52 +1200

    [master] adds backup logs script, a coronjob for bkkp logs

---
+++ config-files/backup_logs.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Title
+#          Description
+# ------------------------------------------------------------------
+
+VERSION=0.1.0
+SUBJECT=backup_logs
+
+function print_usage()
+{
+    echo "Usage: backup_logs.sh [-hv] BUCKET_NAME ENVIRONMENT"
+  	echo -e "Parameters: bucket \n\t BUCKET_NAME: the bucket name in S3 \n\t ENVIRONMENT: use \"\" for production \n"
+    echo -e "\n Use -h for help"
+}
+
+# --- Options processing -------------------------------------------
+if [ $# == 0 ] ; then
+    print_usage
+    exit 1;
+fi
+
+while getopts ":vh" optname
+  do
+    case "$optname" in
+      "v")
+        echo "Version $VERSION"
+        exit 0;
+        ;;
+      # "i")
+      #   echo "-i argument: $OPTARG"
+      #   ;;
+      "h")
+        print_usage
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+shift $(($OPTIND - 1))
+
+bucketName=$1
+environment=$2
+
+
+# --- Locks -------------------------------------------------------
+LOCK_FILE=/tmp/$SUBJECT.lock
+if [ -f "$LOCK_FILE" ]; then
+   echo "Script is already running"
+   exit
+fi
+
+trap "rm -f $LOCK_FILE" EXIT
+touch $LOCK_FILE
+
+# --- Body --------------------------------------------------------
+
+# capture environment parameter if present
+environment="" # Set to "testing" if you're in testing env
+if [[ ! -z "$environment" ]]; then environment="-$environment"; fi
+echo "### Environment: $environment"
+
+
+# upload log file to S3 bucket (sse AES256 encryption and intelligent tiering)
+aws s3 mv ./logs-bkp/* s3://$bucketName/atomiCoconut$environment/logs --sse --storage-class INTELLIGENT_TIERING
